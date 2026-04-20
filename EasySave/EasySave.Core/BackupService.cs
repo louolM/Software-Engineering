c5ae2658ba8c6aaaa2762e7e6857ded0a8b3a051
ReynaD1ff <weyandtjules@gmail.com>
2026-04-20 12:26:07 +0200

    Add testsource and testtarget file copy = ok

---
+++ EasySave/EasySave.Core/BackupService.cs
@@ -0,0 +1,52 @@
+﻿using EasyLog;
+using EasySave.Infrastructure;
+
+namespace EasySave.Core;
+
+public class BackupService
+{
+    private readonly FileService _fileService = new();
+    private readonly Logger _logger = new();
+
+    public void RunBackup(BackupJob job)
+    {
+        var files = _fileService.GetAllFiles(job.SourcePath);
+
+        foreach (var file in files)
+        {
+            try
+            {
+                var relativePath = Path.GetRelativePath(job.SourcePath, file);
+                var targetFile = Path.Combine(job.TargetPath, relativePath);
+
+                var start = DateTime.Now;
+
+                _fileService.CopyFile(file, targetFile);
+
+                var duration = (long)(DateTime.Now - start).TotalMilliseconds;
+
+                _logger.Write(new LogEntry
+                {
+                    Timestamp = DateTime.Now,
+                    BackupName = job.Name,
+                    SourcePath = file,
+                    TargetPath = targetFile,
+                    FileSize = new FileInfo(file).Length,
+                    TransferTime = duration
+                });
+            }
+            catch
+            {
+                _logger.Write(new LogEntry
+                {
+                    Timestamp = DateTime.Now,
+                    BackupName = job.Name,
+                    SourcePath = file,
+                    TargetPath = "",
+                    FileSize = 0,
+                    TransferTime = -1
+                });
+            }
+        }
+    }
+}
