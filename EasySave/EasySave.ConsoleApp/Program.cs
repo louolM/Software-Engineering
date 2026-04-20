4ed8603132e7ceba7fcdc3ab0561a7aba8c01839
ReynaD1ff <weyandtjules@gmail.com>
2026-04-20 12:26:07 +0200

    Add testsource and testtarget file copy = ok

--- EasySave/EasySave.ConsoleApp/Program.cs
+++ EasySave/EasySave.ConsoleApp/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using EasySave.Core;
+
+var service = new BackupService();
+
+var job = new BackupJob
+{
+    Id = 1,
+    Name = "TestBackup",
+    SourcePath = "C:\\TestSource",
+    TargetPath = "C:\\TestTarget",
+    Type = BackupType.Full
+};
+
+service.RunBackup(job);
