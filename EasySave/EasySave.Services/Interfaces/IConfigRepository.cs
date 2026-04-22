619773f8ac8b5c7eec9b61f092e290e32c82af7f
Klimouun <Clement.marques.work@proton.me>
2026-04-22 15:15:23 +0200

    Comments - IConfigRepository.cs

--- EasySave/EasySave.Services/Interfaces/IConfigRepository.cs
+++ EasySave/EasySave.Services/Interfaces/IConfigRepository.cs
@@ -2,6 +2,9 @@
 
 namespace EasySave.Services.Interfaces;
 
+// Defines the contract for loading and saving the list of backup jobs.
+//
+// Implementations decide how and where jobs are persisted (JSON file, database, etc.).
 public interface IConfigRepository
 {
     List<BackupJob> Load();
