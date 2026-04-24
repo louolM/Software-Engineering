54399303927ec55d8f1762d9a1d31c5535437a74
louolM <maurerwassmerlou@gmail.com>
2026-04-24 14:19:38 +0200

    Just projet structure

---
+++ deliverable_2/deliverable_2/ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace deliverable_2
+{
+    class ViewModel : ObservableObject
+    {
+
+    }
+}
