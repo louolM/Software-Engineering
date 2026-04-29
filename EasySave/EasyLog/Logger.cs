f68ab17ce38f688a84b4d26b085db89b23c5e912
klimouun <Clement.marques@gmail.com>
2026-04-29 10:34:45 +0200

    Error - Revert previous commit

--- EasySave/EasyLog/Logger.cs
+++ EasySave/EasyLog/Logger.cs
@@ -20,7 +20,7 @@ public class Logger
 
     public Logger(string format = "JSON")
     {
-        _format = format.ToUpper() == "XML" ? "XML" : "JSON (Default)";
+        _format = format.ToUpper() == "XML" ? "XML" : "JSON";
     }
 
     public void Write(LogEntry entry)
